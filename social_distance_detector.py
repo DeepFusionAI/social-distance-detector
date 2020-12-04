9821fc643a48a472366a90d88df777402b5fa4d7
adhya11720 <69643060+adhya11720@users.noreply.github.com>
2020-12-04 23:43:17 +0530

    Import the necessary library modules
    We will need these modules while working on this social distance detector project.

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -1,1 +1,9 @@
+from pyimagesearch import social_distancing_config as config
+from pyimagesearch.detection import detect_people
+from scipy.spatial import distance as dist
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
 
