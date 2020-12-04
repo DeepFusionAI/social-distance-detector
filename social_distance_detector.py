7de5556628da0e4146ce70ded52ed5796fbca831
adhya11720 <69643060+adhya11720@users.noreply.github.com>
2020-12-05 00:47:58 +0530

    Load the COCO class labels
    Load the COCO class labels on which YOLO model will be trained on during the project

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -7,3 +7,15 @@ import imutils
 import cv2
 import os
 
+
+#Load the COCO class labels on which YOLO model will be trained on
+labelsPath = os.path.sep.join([config.MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+ 
+weightsPath = os.path.sep.join([config.MODEL_PATH, "yolov3.weights"])
+configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
+ 
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
