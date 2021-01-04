de4166c6b791786d3549a2c25a6ae86b557da1c9
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2021-01-04 20:00:05 +0530

    label loading

---
+++ deepfusionai/label.py
@@ -0,0 +1,14 @@
+path="../label/coco_labels_1.txt"
+
+def load_labels(path):
+  """Loads the labels file. Supports files with or without index numbers."""
+  with open(path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    labels = {}
+    for row_number, content in enumerate(lines):
+      pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+      if len(pair) == 2 and pair[0].strip().isdigit():
+        labels[int(pair[0])] = pair[1].strip()
+      else:
+        labels[row_number] = pair[0].strip()
+  return labels
